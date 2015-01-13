74b463be23ef7166b070c9150a80a3022032f286
adwd <adwd118@gmail.com>
2015-01-13 10:04:38 +0900

    1.3

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -1,12 +1,23 @@
 package controllers
 
+// Play全体に関する機能、アプリケーション設定や
+// ログ出力、ルーティングなどに関するクラス
 import play.api._
+
+// MVCの諸機能に関するクラス群
 import play.api.mvc._
+
+// Viewでのヘルパー等に関するクラス
+// import play.api.views._
+
+import views._
 
 object Application extends Controller {
 
+  // アクションはResultを返す
   def index = Action {
-    Ok(views.html.index("Your new application is ready."))
+    // Okはアクセスが正常に完了したResultを返す
+    // index.scala.htmlテンプレートに引数を渡して描画する
+    Ok(html.index("Your new application is ready."))
   }
-
 }
