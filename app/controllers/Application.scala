47ef209d846e5101d0094fa8b11b801c9998810c
adwd <adwd118@gmail.com>
2015-03-31 08:02:17 +0900

    to 3.1.4

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -10,70 +10,12 @@ import play.api.mvc._
 // Viewでのヘルパー等に関するクラス
 // import play.api.views._
 
-import play.api.data.Form
-import play.api.data.Forms._
-
-import play.api.db.slick._
-import play.api.data.validation.Constraints.{pattern}
-import java.sql.Timestamp
-
 import views._
 import models._
 
 object Application extends Controller {
-
-  val memberForm = Form(
-		mapping(
-  		"id" -> longNumber(min = 0, max = 1000),
-  	  "name" -> nonEmptyText,
-  	  "mail" -> nonEmptyText,
-			"tel" -> nonEmptyText
-	  )(Member.apply)(Member.unapply)
-	)
-
-	val messageForm = Form(
-		mapping(
-			"id" -> longNumber(min = 0, max = 1000),
-			"memberid" -> longNumber,
-			"name" -> nonEmptyText.verifying(pattern("""[0-9a-zA-Z]+""".r,
-				error = "半角英数だけで入力してください。")),
-			"message" -> nonEmptyText.verifying("メッセージエラー", msg => msg.length < 140),
-			"postdate" -> ignored(new Timestamp(new java.util.Date().getTime()))
-		)(Message.apply)(Message.unapply)
-	)
-
-  def index = DBAction { implicit rs =>
-  	val messages = MessageDAO.all
-		val members = MemberDAO.all
-    Ok(html.index("データベースのサンプル", messages, members))
+  def index = Action {
+    Ok("<html><body><h1>Hello</h1><p>This is test.</p></body></html>")
+    .as("text/xml")
   }
-
-  def addMessage = Action {
-  	Ok(html.addmessage("投稿フォーム", messageForm))
-  }
-
-	def addMember = Action {
-		Ok(html.addmember("メンバー登録フォーム", memberForm))
-	}
-
-  def createMessage = DBAction { implicit rs =>
-  	messageForm.bindFromRequest.fold(
-  		errors => BadRequest(html.addmessage(errors.errors.toString, errors)),
-  		message => {
-  			MessageDAO.create(message)
-  			Redirect(routes.Application.index)
-      }
-    )
-  }
-
-	def createMember = DBAction { implicit rs =>
-		memberForm.bindFromRequest.fold(
-			errors => BadRequest(html.addmember(errors.errors.toString, errors)),
-			member => {
-				MemberDAO.create(member)
-				Redirect(routes.Application.index)
-			}
-		)
-	}
-
 }
