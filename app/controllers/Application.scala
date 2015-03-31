6c9998642dbb672031297991108658cd79c30507
adwd <adwd@users.noreply.github.com>
2015-03-31 19:04:35 +0900

    ch 3.2.4 WIP

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -3,6 +3,8 @@ package controllers
 // Play全体に関する機能、アプリケーション設定や
 // ログ出力、ルーティングなどに関するクラス
 import play.api._
+import play.api.data.Form
+import play.api.data.Forms._
 
 // MVCの諸機能に関するクラス群
 import play.api.mvc._
@@ -14,12 +16,23 @@ import views._
 import models._
 
 object Application extends Controller {
-  def index = Action { request =>
-    request.method match {
-      case "POST" =>
-        val msg = request.body.asFormUrlEncoded.get("input")(0)
-        Ok(html.index(s"posted: $msg"))
-      case _ => Ok(html.index("please type"))
-    }
+
+	val sampleform = Form(
+		tuple(
+			"input" -> text,
+			"pass"  -> text,
+			"check" -> boolean,
+			"radio" -> text,
+			"sel"   -> text,
+			"area"  -> text,
+			"date"  -> date
+		)
+	)
+
+  def index = Action {
+		val form = sampleform.fill("default value", "", true, "windows", "uk", "", null)
+		Ok(html.index("please fill form", form))
   }
+
+	def send = TODO
 }
